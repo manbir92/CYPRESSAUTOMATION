ab43c77b2e460bf2420124d0104cca1d738f6f45
psycho <psycho@psychos-MacBook-Air.local>
2023-04-02 22:01:06 -0400

    my first commit

--- cypress/integration/2-advanced-examples/cypressautomation/classproject.spec.js
+++ cypress/integration/2-advanced-examples/cypressautomation/classproject.spec.js
@@ -21,4 +21,29 @@ it('Loginpage', function ()
     // find element by using cypress playground
 
     cy.get('.subtext').should('contain','MANBIR')
+
+    //Add product in cart
+
+    cy.xpath('//*[@id="categorymenu"]/nav/ul/li[2]/a').click()
+    cy.xpath('//*[@id="maincontainer"]/div/div/div/div/ul/li[2]/div/a').click()
+    cy.xpath('//*[@id="maincontainer"]/div/div/div/div/div[3]/div[6]/div[2]/div[3]/a').click()
+    cy.xpath('//*[@id="product"]/fieldset/div[6]/ul/li/a').click()
+    cy.xpath('//*[@id="cart_checkout1"]').click()
+    cy.xpath('//*[@id="checkout_btn"]').click()
+    
+    //Assertion for order conformation
+
+    cy.get('div').should('contain','Thank you for shopping with us!')
+    cy.xpath('//*[@id="maincontainer"]/div/div/div/div/section/p[3]/a').click()
+
+    //Assertion for invoice (order details)
+
+    cy.get('div').should('contain','Order ID')
+    cy.get('div').should('contain','Shipping Address')
+    cy.get('div').should('contain','Payment Address')
+
+    cy.xpath('//*[@id="maincontainer"]/div/div[1]/div/div/div[4]/div/a[1]').click()
+    cy.xpath('//*[@id="categorymenu"]/nav/ul/li[1]/a').click()
+    
+
 })
